187f9c382c474595a4f1d26c0b9670be3b00c696
Angelika <jurekangelika8@gmail.com>
2024-04-08 20:33:02 +0200

    Add_section_with_data_to_appsettings.Development
    Dodanie sekcji z danymi oraz ustawienie w Program.cs app.UseCors("CorsPolicy");

--- backend/Configurations/CorsConfig.cs
+++ backend/Configurations/CorsConfig.cs
@@ -4,7 +4,7 @@ public sealed class CorsConfig
 {
     public bool AllowCredentials { get; set; }
     public IEnumerable<string> AllowedOrigins { get; set; }
-    public IEnumerable<string> AllowedMethods { get; set; }
+    public IEnumerable<string> AllowedMethods { get; set; }// Lista metod HTTP, które są dozwolone w żądaniach CORS. Na przykład: GET, POST, PUT, DELETE, itp.
     public IEnumerable<string> AllowedHeaders { get; set; }
-    public IEnumerable<string> ExposedHeaders { get; set; }
+    public IEnumerable<string> ExposedHeaders { get; set; }// Lista nagłówków HTTP, które są dozwolone w odpowiedziach CORS
 }
