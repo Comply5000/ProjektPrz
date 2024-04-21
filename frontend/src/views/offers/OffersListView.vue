5a35d88baec5622d7a10e5ba5e57e51a8ba7b99d
guzdziu <kguzdek7@gmail.com>
2024-04-21 23:50:49 +0200

    lista ofert wraz z przykładowymi ofertami

--- frontend/src/views/offers/OffersListView.vue
+++ frontend/src/views/offers/OffersListView.vue
@@ -1,32 +1,49 @@
 <template>
     <div class="offer-container">
-      <!-- Formularz wyszukiwania nad tablicą ofert -->
+      <!-- Formularz wyszukiwania -->
       <div class="search-form">
         <input v-model="searchQuery" class="form-control" type="search" placeholder="Search" aria-label="Search">
       </div>
-      <!-- Białe tło z listą ofert -->
+      <!-- Lista ofert -->
       <div class="offer-list-container">
         <div class="offer-list">
           <ul>
             <li v-for="(offer, index) in paginatedOffers" :key="index" class="offer-item">
               <div class="offer-details">
-                <div class="offer-image">
-                  <img src="../offers/flamingo.jpg" alt="Offer Image">
-                </div>
                 <div class="offer-content">
-                  <h3>{{ offer.title }}</h3>
-                  <p>{{ offer.description }}</p>
+                  <div class="offer-text">
+                    <h3 class="offer-name">{{ offer.offerName }}</h3>
+                    <p class="company-name">{{ offer.companyName }}</p>
+                    <p>Typ: <span class="offer-type">{{ offer.companyType }}</span></p>
+                    <p>Data wystawienia: {{ offer.issueDate }}</p>
+                    <p>Data ważności: {{ offer.expiryDate }}</p>
+                  </div>
+                </div>
+                <div class="offer-image">
+                  <img :src= "offer.image" :alt="'Offer Image ' + (index + 1)">
                 </div>
               </div>
             </li>
           </ul>
         </div>
-  
+        <!-- Wybór liczby ofert na stronie -->
+        <div class="offer-per-page">
+          <label for="offerPerPage">Oferty na stronie:</label>
+          <select v-model="pageSize" id="offerPerPage">
+            <option v-for="option in pageSizeOptions" :value="option">{{ option }}</option>
+          </select>
+        </div>
         <!-- Paginacja -->
-        <nav aria-label="Page navigation example" class="pagination-container">
+        <nav aria-label="Page navigation example">
           <ul class="pagination justify-content-center">
-            <li v-for="(page, index) in pageCount" :key="index" class="page-item" @click="changePage(page)">
-              <a class="page-link" href="#">{{ page }}</a>
+            <li class="page-item">
+              <button class="page-link" @click="changePage(currentPage - 1)" :disabled="currentPage === 1">Previous</button>
+            </li>
+            <li class="page-item" v-for="page in pageCount" :key="page">
+              <a class="page-link" href="#" @click="changePage(page)">{{ page }}</a>
+            </li>
+            <li class="page-item">
+              <button class="page-link" @click="changePage(currentPage + 1)" :disabled="currentPage === pageCount">Next</button>
             </li>
           </ul>
         </nav>
@@ -39,16 +56,16 @@
     data() {
       return {
         currentPage: 1,
-        pageSize: 5, // liczba ofert na stronie
-        searchQuery: '', // zmienna do przechowywania wyszukiwanej frazy
-        offers: [ // przykładowe oferty
-          { title: 'Oferta 1', description: 'Opis oferty 1', image: 'path/do/zdjecia1.jpg' },
-          { title: 'Oferta 2', description: 'Opis oferty 2', image: 'path/do/zdjecia2.jpg' },
-          { title: 'Oferta 3', description: 'Opis oferty 3', image: 'path/do/zdjecia3.jpg' },
-          { title: 'Oferta 4', description: 'Opis oferty 4', image: 'path/do/zdjecia4.jpg' },
-          { title: 'Oferta 5', description: 'Opis oferty 5', image: 'path/do/zdjecia5.jpg' },
-          { title: 'Oferta 6', description: 'Opis oferty 6', image: 'path/do/zdjecia6.jpg' },
-          // Dodaj więcej ofert, jeśli potrzebujesz
+        pageSize: 5,
+        pageSizeOptions: [5, 10, 15],
+        searchQuery: '',
+        offers: [
+          { offerName: 'Oferta A', companyName: 'Firma A', companyType: 'Gastronomia', issueDate: '2024-04-21', expiryDate: '2024-05-21', image: "../src/views/offers/flamingo.jpg" },
+          { offerName: 'Oferta B', companyName: 'Firma B', companyType: 'Usługi', issueDate: '2024-04-20', expiryDate: '2024-05-20', image: "../src/views/offers/flamingo.jpg" },
+          { offerName: 'Oferta C', companyName: 'Firma C', companyType: 'Wynajem', issueDate: '2024-04-19', expiryDate: '2024-05-19', image: "../src/views/offers/flamingo.jpg" },
+          { offerName: 'Oferta D', companyName: 'Firma D', companyType: 'Gastronomia', issueDate: '2024-04-18', expiryDate: '2024-05-18', image: "../src/views/offers/flamingo.jpg" },
+          { offerName: 'Oferta E', companyName: 'Firma E', companyType: 'Usługi', issueDate: '2024-04-17', expiryDate: '2024-05-17', image: "../src/views/offers/flamingo.jpg" },
+          { offerName: 'Oferta F', companyName: 'Firma F', companyType: 'Wynajem', issueDate: '2024-04-16', expiryDate: '2024-05-16', image: "../src/views/offers/flamingo.jpg" }
         ]
       };
     },
@@ -59,10 +76,9 @@
         return this.filteredOffers.slice(startIndex, endIndex);
       },
       filteredOffers() {
-        // Filtruj oferty na podstawie wyszukiwanej frazy
         return this.offers.filter(offer =>
-          offer.title.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
-          offer.description.toLowerCase().includes(this.searchQuery.toLowerCase())
+          offer.offerName.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+          offer.companyName.toLowerCase().includes(this.searchQuery.toLowerCase())
         );
       },
       pageCount() {
@@ -71,7 +87,9 @@
     },
     methods: {
       changePage(page) {
-        this.currentPage = page;
+        if (page >= 1 && page <= this.pageCount) {
+          this.currentPage = page;
+        }
       }
     }
   };
@@ -79,60 +97,85 @@
   
   <style scoped>
   .offer-container {
-    background-color: #28a745; /* Kolor tła poza tablicą */
+    background-color: #28a745;
     height: 100vh;
     display: flex;
-    justify-content: center;
     align-items: center;
-    flex-direction: column; /* Ustawienie kolumnowo dla flexbox */
+    flex-direction: column;
   }
   
   .search-form {
-    margin-bottom: 20px; /* Dodatkowy margines na dole pomiędzy formularzem wyszukiwania a listą ofert */
-    width: 30%; /* Ustawienie szerokości formularza na 75% */
+    margin-top:10px;
+    margin-bottom: 10px;
+    width: 30%;
   }
   
   .offer-list-container {
     width: 75%;
-    overflow: auto; /* Dodanie przewijania, jeśli lista ofert przekracza wysokość */
+    overflow: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
   
   .offer-list {
-    background-color: #73c987;
+    background-color: #ddd;
     padding: 20px;
-    overflow-y: auto; /* Dodanie przewijania, jeśli lista ofert przekracza wysokość */
+    overflow-y: auto;
+    width: 100%;
   }
   
   .offer-item {
     list-style-type: none;
     margin-bottom: 20px;
-    background-color: #28a745;
+    background-color: white;
+    padding: 10px;
+    border-radius: 5px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
   
-  img{
-    width:50px;
-    height:50px;
-  }
-
   .offer-details {
     display: flex;
-    justify-content: space-between; /* Rozłożenie elementów na całej szerokości */
-    align-items: center; /* Wyśrodkowanie w pionie */
+    justify-content: space-between;
+    width: 100%;
   }
   
-  .offer-details .offer-content {
-    flex-grow: 1; /* Elastyczność dla treści, aby zajęła jak najwięcej miejsca */
-    text-align: center; /* Wyśrodkowanie tekstu na środku */
+  .offer-content {
+    flex-grow: 1;
   }
   
-  .offer-details .offer-image {
-    margin-right: 20px; /* Odstęp od treści */
-    border: 4px solid black; /* Czarna ramka */
-    padding: 5px; /* Dodatkowy padding dla ramki */
+  .offer-text {
+    text-align: center;
   }
-  .offer-details .offer-image img {
-    max-height: 100%; /* Maksymalna wysokość obrazka */
-    max-width: 100%; /* Maksymalna szerokość obrazka */
+  
+  .offer-text h3,
+  .offer-text p {
+    margin: -4px;
   }
+  
+  .offer-type {
+    padding: 5px 10px;
+    border-radius: 5px;
+    text-align: center;
+  }
+  
+  .offer-name {
+    background-color: #28a745;
+    color: white;
+    padding: 5px 10px;
+    border-radius: 5px;
+    text-align: center;
+  }
+  
+  .offer-image img {
+    width: 110px;
+    height: 110px;
+    margin-top: -4px;
+    border-radius: 10px;
+    border: 5px solid #28a745; /* Zielona ramka o szerokości 2px */
+}
+
   </style>
   
