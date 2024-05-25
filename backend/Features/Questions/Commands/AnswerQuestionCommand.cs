11cfe9464366788d86495606e01c3c382139bf7e
Angelika <jurekangelika8@gmail.com>
2024-05-25 19:12:21 +0200

    Question_create_answer

---
+++ backend/Features/Questions/Commands/AnswerQuestionCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+
+namespace API.Features.Questions.Commands
+{
+    public class AnswerQuestionCommand : IRequest<Guid>
+    {
+        public Guid QuestionId { get; set; }
+        public string Answer { get; set; }
+        public Guid UserId { get; set; } // kto udzielil odpowiedzi
+    }
+}
---
+++ backend/Features/Questions/Commands/ValidatorAnswerQuestion.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using API.Features.Questions.Commands;
+
+namespace API.Features.Questions.Validators
+{
+    public class AnswerQuestionValidator : AbstractValidator<AnswerQuestionCommand>
+    {
+        public AnswerQuestionValidator()
+        {
+            RuleFor(x => x.QuestionId).NotEmpty().WithMessage("QuestionId is required");
+            RuleFor(x => x.Answer).NotEmpty().WithMessage("Answer is required");
+            RuleFor(x => x.UserId).NotEmpty().WithMessage("UserId is required");
+        }
+    }
+}
