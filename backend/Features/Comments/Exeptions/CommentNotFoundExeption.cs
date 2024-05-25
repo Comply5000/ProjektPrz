82e8b445cc1de8cb9ca4b5a493066392fc89aac0
Comply5000 <wojciech.kaczor.comply@gmail.com>
2024-05-25 23:07:58 +0200

    fix tego syfu

---
+++ backend/Features/Comments/Exeptions/CommentNotFoundExeption.cs
@@ -0,0 +1,12 @@
+﻿using API.Common.Exceptions;
+
+namespace API.Features.Comments.Exeptions;
+
+public class CommentNotFoundExeption : BaseException
+{
+    public CommentNotFoundExeption() : base("comment doesn't exist")
+    {
+    }
+}
+
+
