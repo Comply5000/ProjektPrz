3df21298e4645a7600d94eb92f7e427c7a721a03
guzdziu <kguzdek7@gmail.com>
2024-06-11 16:54:45 +0200

    zrobiona moja oferta

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -109,6 +109,14 @@ const router = createRouter({
         component: () => import('../views/companies/CompanyView.vue')
     },
     {
+      path: '/company-offers-list',
+        name: 'company-offers-list',
+        // route level code-splitting
+        // this generates a separate chunk (About.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import('../views/companies/CompanyOffersList.vue')
+    },
+    {
       path: '/company-list',
       name: 'company-list',
       // route level code-splitting
